1630849a6844a60d876b42e94482d46e48383f26
Titouan-dld <titouan-douillard@gmail.com>
2026-04-18 17:27:36 +0200

    debut

---
+++ src/chiffresRomains/ChiffresRomains.java
@@ -0,0 +1,5 @@
+package chiffresRomains;
+
+public class ChiffresRomains {
+
+}
---
+++ test/test/TestChiffresRomains.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestChiffresRomains {
+
+	ChiffresRomains chiffresRomains;
+	
+	@BeforeEach
+	void initChiffresRomains() {
+		chiffresRomains = ChiffresRomains();
+	}
+	
+	@Test
+	void test1()
+	
+}
